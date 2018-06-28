8e4ca9ad03f482f08df7ac26d51039168a6828a8
shalomz <shalom.nyende@gmail.com>
2018-06-29 01:41:17 +0300

    Gitignore Introduced, and structure clean up

--- src/faq/utils.py
+++ src/faq/utils.py
@@ -6,6 +6,9 @@ import json
 
 
 def get_questions():
+    """
+        Fetch Questions from eosio.stackexchange
+    """
     count = 1
     for i in range(6):
         r = requests.get('https://api.stackexchange.com/2.2/questions?filter=withbody&site=eosio&pagesize=100&page={}'.format(count))
@@ -13,7 +16,6 @@ def get_questions():
         for item in data['items']:
             own = item['owner']['user_id']
             dsp = item['owner']['display_name']
-            print(own, dsp)
             try:
                 owner = User.objects.get(username=own, se_display_name=dsp)
             except Exception:
@@ -23,7 +25,6 @@ def get_questions():
             if owner:
                 for tag in tags:
                     t, created = Tag.objects.get_or_create(name=tag)
-                    print(created)
                     ts.append(t)
                 q = Question.objects.create(owner=owner, se_question_id=item['question_id'], title=item['title'], body=item[
                                             'body'], se_link=item['link'], is_answered=item['is_answered'], score=item['score'])
@@ -35,16 +36,17 @@ def get_questions():
 
 
 def get_answers():
+    """
+        Fetch Answers from Stackexchange
+    """
     count = 1
-    for i in range(200):
+    for i in range(200):  # TODO : Fetch number of all items first
         r = requests.get('http://api.stackexchange.com/2.2/answers?site=eosio&filter=!b1MMEb*6iF.PM5&pagesize=100&page={}'.format(count))
         data = json.loads(r.text)
-
         for item in data['items']:
             own = item['owner']['user_id']
             dsp = item['owner']['display_name']
             qn_id = item['question_id']
-            print(own, dsp)
             try:
                 owner = User.objects.get(username=own, se_display_name=dsp)
                 question = Question.objects.get(se_question_id=qn_id)
@@ -52,8 +54,9 @@ def get_answers():
                 owner = None
                 question = None
             if owner and question:
-                a = Answer.objects.create(owner=owner, question=question, body=item['body'],
-                                          se_question_id=qn_id, is_accepted=item['is_accepted'], se_answer_id=item['answer_id'], score=item['scor    e'])
+                Answer.objects.create(owner=owner, question=question, body=item['body'],
+                                      se_question_id=qn_id, is_accepted=item['is_accepted'],
+                                      se_answer_id=item['answer_id'], score=item['score'])
 
         count += 1
         print(count)
